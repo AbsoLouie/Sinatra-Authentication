b35d80dd227583d70437fc7cd257c5adf3a063f1
Dominic Scotto <dominicscotto@gmail.com>
2013-11-03 15:59:08 -0800

    test

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -19,8 +19,9 @@ post '/sessions' do
    @user = User.find_by_email(params[:email])
     if @user.password == params[:password]
       session[:id] =@user.id
-    else
       redirect '/'
+    else
+      redirect '/sessions/new'
     end
 end
 

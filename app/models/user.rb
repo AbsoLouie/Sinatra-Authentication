1d8e8865f21db55a38629b546103dcb1c53b8272
Louie Perez <louie.perez3@gmail.com>
2013-11-03 15:57:30 -0800

    Included BCrypt in User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ActiveRecord::Base
-  validates :email, uniqueness: true
-  validates :email, format: { with: /\w{2,}[@]\w{2,}[.]\w{2,}/, message: "Improper Email Address" }
+	validates :email, uniqueness: true
+	validates :email, format: { with: /\w{2,}[@]\w{2,}[.]\w{2,}/, message: "Improper Email Address" }
+
+	include BCrypt
+
+	def password
+		@password ||= Password.new(password_hash)
+	end
+
+	def password=(new_password)
+		@password = Password.create(new_password)
+		self.password_hash = @password
+	end
 end
